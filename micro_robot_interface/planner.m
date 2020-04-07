80e6a4cb812f68852d19a54de17336e86339800c
jiayin <darrenjiayinxie@gmail.com>
2020-04-07 12:46:45 -0400

    updates

--- micro_robot_interface/planner.m
+++ micro_robot_interface/planner.m
@@ -4,6 +4,28 @@ A.N = N;
 
 A.Impulses = zeros(N,6);
 
+%% external impulses and angular momentums
+for i = 1:N
+    P_x = 0;
+    P_y = 0;
+    P_z = 0;
+    P_xt = 0;
+    P_yt = 0;
+    P_zt = 0;
+    
+    A.Impulses(i,1) = P_x;
+    A.Impulses(i,2) = P_y;
+    A.Impulses(i,3) = P_z;
+    A.Impulses(i,4) = P_xt;
+    A.Impulses(i,5) = P_yt;
+    A.Impulses(i,6) = P_zt;
+    
+    
+    
+end
+
+
+
 
 
 
